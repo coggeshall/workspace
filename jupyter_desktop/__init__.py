850a7fc85037e050515d781b04bd77a36f5a2905
Jim Coggeshall <jim@jimcoggeshall.com>
2022-05-31 20:25:32 -0700

    test

--- jupyter_desktop/__init__.py
+++ jupyter_desktop/__init__.py
@@ -4,12 +4,14 @@ import os
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 def setup_desktop():
+    with open("/tmp/vncpw", "r") as f:
+        password = f.readline().strip()
     return {
         'command': [os.path.join(HERE, 'bin/startup')],
         'port': 6901,
         'timeout': 30,
         'mappath': {
-            '/': '/vnc.html?path=desktop%2Fwebsockify&resize=remote&autoconnect=true',
+            '/': '/vnc_lite.html?path=desktop%2Fwebsockify&resize=remote&autoconnect=true#password=' + password,
         },
         'new_browser_window': True
     }
