b9caf98c1bb3a85fe7b1cc23ca9e7ddd48c9e346
Jim Coggeshall <jimcoggeshall@gmail.com>
2022-11-29 12:22:46 -0800

    Update poststart.sh

--- poststart.sh
+++ poststart.sh
@@ -9,3 +9,15 @@ cp /opt/install/etc/bash.bashrc /home/jovyan/.bashrc
 ln -s .bashrc /home/jovyan/.bash_profile
 
 sed -i -e "s/RFBSECRET/$PASSWORD/g" /tmp/www/core/rfb.js
+
+cat << EOF > /home/jovyan/extra.sh
+#!/usr/bin/env bash
+
+pushd /home/jovyan
+
+flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+popd
+
+EOF
+chmod +x /home/jovyan/extra.sh
